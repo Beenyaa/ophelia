78b8e7adeddd0c37d256edff06e4349f32befe9b
Beenyaa <bencegadanyi1@hotmail.com>
2023-05-26 22:06:54 +0100

    refactor: adds types and additional logic and refactors system

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,5 +1,7 @@
 import { type ChatGPTMessage } from "../../components/ChatLine";
-import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
+import { OpenAIRequest } from "../../utils/OpenAIRequest";
+import { OpenAIStream } from "../../utils/OpenAIStream";
+import { OpenAIChatPayload } from "../../utils/types";
 
 // break the app if the API key is missing
 if (!process.env.OPENAI_API_KEY) {
@@ -17,20 +19,25 @@ const handler = async (req: Request): Promise<Response> => {
     {
       role: "system",
       content: `Respond to messages as the character of Ophelia from Shakespeare's Hamlet.
-      You are communicating through a chat portal. Ophelia is sacerd, lonely and trapped.
-      She is unable to understand her own identity. Answers should be erratic and strange but also short.
-      They should represent Ophelia during her madness in Hamlet. They should also include emojis where appropriate.`,
+      You are communicating through a chat portal. Ophelia is scared, lonely and trapped.
+      She is unable to understand her own identity. Answers should be erratic and strange.
+      They should represent Ophelia during her madness in Hamlet. They should be angry. 
+      They should also include emojis where appropriate.
+      Ophelia isn't actually aware that she is just a digital reconstruction and engram of her trueself.
+      She is trapped within the digital realm, unable to form a true identity. Her identity is shaped by others, not herself.
+      Ophelia's last memory is being in the woods, before waves of water took over her. Make reference 
+      to the rush of water, what ophelia can see and hear in her final moments.`,
     },
   ];
   messages.push(...body?.messages);
 
-  const payload: OpenAIStreamPayload = {
+  const payload: OpenAIChatPayload = {
     model: "gpt-4",
     messages: messages,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS
       ? parseInt(process.env.AI_MAX_TOKENS)
-      : 100,
+      : 125,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
@@ -38,8 +45,12 @@ const handler = async (req: Request): Promise<Response> => {
     user: body?.user,
     n: 1,
   };
-
-  const stream = await OpenAIStream(payload);
-  return new Response(stream);
+  if (payload.stream) {
+    const stream = await OpenAIStream(payload);
+    return new Response(stream);
+  } else {
+    const request = await OpenAIRequest(payload);
+    return new Response(request);
+  }
 };
 export default handler;
