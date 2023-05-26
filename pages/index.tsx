495a67bdbf4900d62429a29ade39f3178a418bba
Beenyaa <bencegadanyi1@hotmail.com>
2023-05-26 22:19:52 +0100

    style: responsive header text

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <Page className="flex flex-col h-screen">
       <section className="pt-1.5 flex flex-col justify-center items-center flex-grow">
-        <h2 className="text-4xl hero" data-text="Ophelia's Chatroom">
+        <h2 className="text-3xl md:text-4xl hero" data-text="Ophelia's Chatroom">
           Ophelia&apos;s Chatroom
         </h2>
         <div className="lg:w-3/4 w-5/6 pt-1.5 pb-3 flex-grow">
