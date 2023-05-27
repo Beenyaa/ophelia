366a32f2d8c8ee8d37a324bab1e986c4142a93dd
Beenyaa <bencegadanyi1@hotmail.com>
2023-05-27 16:46:55 +0100

    feat: new styling and glitch effect

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -19,7 +19,7 @@ const InputMessage = ({ input, setInput, sendMessage }: any) => (
       type="text"
       aria-label="chat input"
       required
-      className="min-w-0 flex-auto appearance-none rounded-md border border-pink-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-pink-800/5 placeholder:text-pink-400 focus:border-rose-500 focus:outline-none focus:ring-4 focus:ring-rose-500/10 sm:text-sm"
+      className="min-w-0 flex-auto appearance-none rounded-md border border-pink-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-pink-800/5 placeholder:text-black text-black focus:border-rose-500 focus:outline-none focus:ring-4 focus:ring-rose-500/10 sm:text-sm"
       value={input}
       onKeyDown={(e) => {
         if (e.key === "Enter") {
@@ -125,7 +125,7 @@ export function Chat() {
   }, [messages]);
 
   return (
-    <div className="rounded-2xl border-zinc-100 border p-6 h-full flex flex-col justify-end">
+    <div className=" bg-pink-50 border-pink-50 border p-6 h-full flex flex-col justify-end">
       <div className="overflow-y-auto" ref={scrollContainerRef}>
         {messages.map(({ content, role }, index) => (
           <ChatLine
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,42 @@
-import { Layout, Text, Page } from "@vercel/examples-ui";
+import { Layout, Page } from "@vercel/examples-ui";
+import Image from "next/image";
 import { Chat } from "../components/Chat";
+import "98.css";
 
 function Home() {
   return (
-    <Page className="flex flex-col h-screen">
-      <section className="pt-1.5 flex flex-col justify-center items-center flex-grow">
-        <h2 className="text-3xl md:text-4xl hero" data-text="Ophelia's Chatroom">
-          Ophelia&apos;s Chatroom
-        </h2>
-        <div className="lg:w-3/4 w-5/6 pt-1.5 pb-3 flex-grow">
-          <Chat />
-        </div>
-      </section>
-    </Page>
+    <>
+      <Image
+        className="background-image"
+        height="100"
+        width="100"
+        src="pink.svg"
+        alt="pink background"
+      />
+      <Page className="flex flex-col h-screen relative ">
+        <section className="flex flex-col justify-center items-center grow py-3">
+          <h2
+            className="text-3xl md:text-4xl hero pb-3"
+            data-text="Ophelia's Chatroom"
+          >
+            Ophelia&apos;s Chatroom
+          </h2>
+          <div className="lg:w-3/4 w-5/6 pt-3 pb-6 grow-0 window h-[90vh]">
+            <div className="title-bar">
+              <div className="title-bar-text">Ophelia&apos;s Chatroom</div>
+              <div className="title-bar-controls">
+                <button aria-label="Minimize" />
+                <button aria-label="Maximize" />
+                <button aria-label="Close" />
+              </div>
+            </div>
+            <div className="grow-0 window-body h-[95%]">
+              <Chat />
+            </div>
+          </div>
+        </section>
+      </Page>
+    </>
   );
 }
 
